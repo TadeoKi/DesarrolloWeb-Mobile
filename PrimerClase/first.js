36a58054bb43874f3b89a7bb3d274e32883b5d77
Tadeo <tadeo.ki@correo.ucu.edu.uy>
2025-03-17 20:33:50 -0300

    primer commit

---
+++ PrimerClase/first.js
@@ -0,0 +1,11 @@
+
+async function main(){
+    const url = "https://openlibrary.org/search.json?q=the+lord+of+the+rings"
+    const req = await fetch(url);
+    if(req.ok){
+        const data = await req.json();
+        console.log(data);
+    }
+}
+
+main();
